e3201bd94a7b47a3fa828c3c839296b7a03135e4
Ezra Obiwale <contact@ezraobiwale.com>
2020-02-14 16:51:08 +0100

    Initial commit: All ready

---
+++ README.md
@@ -0,0 +1,18 @@
+# NPM Library
+
+A template for building node.js npm libaries with webpack and rollup.
+
+## Features
+
+- Handles ES6 transpiling
+- Bundles for browser and commonjs
+- Minimizes built files
+- Publishes on the built files to npmjs.org
+- Excludes the built files from being committed to git
+
+## Usage
+
+1. Change all instances of **library** in the `package.json` file to the name of
+   the library being built.
+2. Write your code on `src/index.js` file.
+3. Run `yarn build` to build files for distribution.
---
+++ rollup.config.js
@@ -0,0 +1,38 @@
+import resolve from '@rollup/plugin-node-resolve'
+import commonjs from '@rollup/plugin-commonjs'
+import babel from 'rollup-plugin-babel'
+import { terser } from 'rollup-plugin-terser'
+import pkg from './package.json'
+
+let name = pkg.name.replace(/[^a-z\/]/gi, '')
+
+if (name.index('/') !== -1) {
+  name = name.split('/')[1]
+}
+
+export default {
+  input: 'src/index.js',
+  output: [
+    {
+      file: pkg.rollup.main,
+      format: 'cjs'
+    },
+    {
+      file: pkg.rollup.module,
+      format: 'es'
+    },
+    {
+      file: pkg.rollup.browser,
+      format: 'iife',
+      name: name
+    }
+  ],
+  plugins: [
+    resolve(),
+    commonjs(),
+    babel({
+      exclude: 'node_modules/**' // only transpile our source code
+    }),
+    terser()
+  ]
+}
